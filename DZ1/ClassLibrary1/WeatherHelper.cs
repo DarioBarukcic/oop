50c266c593c98d0e125892d035cde0b5a891d2fe
dario barukcic <darionarukcic07@gmail.com>
2021-11-07 21:22:31 +0100

    added homework

---
+++ DZ1/ClassLibrary1/WeatherHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherClass
+{
+    public static class WeatherHelper
+    {
+        public static Weather FindWeatherWithLargestWindchill(Weather[] weathers)
+        {
+            double largest = weathers[0].CalculateWindChill();
+            int index=0;
+            for (int i = 0; i < weathers.Length; i++)
+            {
+                if (weathers[i].CalculateWindChill() > largest)
+                {
+                    largest = weathers[i].CalculateWindChill();
+                    index = i;
+                }
+            }
+            return weathers[index];
+        }
+    }
+}
